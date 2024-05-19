5be4adb7d5852225f85b7020ce8aed7c8647202b
arturombc <arturomagno1@gmail.com>
2024-05-19 00:14:33 -0500

    testavl proxy

--- src/proxy/test_proxy.cpp
+++ src/proxy/test_proxy.cpp
@@ -2,11 +2,16 @@
 #include <iostream>
 
 using namespace std;
-int main(){
+
+void testavl(){
     Proxy proxy;
     QueryResult query_result = proxy.execute_query("CREATE TABLE users FROM FILE 'AppleStore.dat' USING INDEX AVL ('price');");
     cout << query_result << endl;
     query_result = proxy.execute_query("SELECT track_name FROM users WHERE price=0;");
     cout << query_result << endl;
+}
+
+int main(){
+    testavl();
     return 0;
 }
